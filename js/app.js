2f3dd3d41c701959418d42915e9f0a4a12401832
Susan S. <susan.sotosarmiento@mygeorgian.ca>
2025-02-18 23:33:54 -0500

    declare variables

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 
 // const for main button query selectors
-const 1-noun =
+const noun1 = document.querySelector("#noun1");
+const verb2 = document.querySelector("#verb2");
+const adj3 = document.querySelector("#adj3");
+const noun4 = document.querySelector("#noun4");
+const comp5 = document.querySelector("#comp5");
+
+//const for other buttons query selectors
+const show = document.querySelector("#show");
+const random = document.querySelector("#random");
